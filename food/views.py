19b6da6968846513e82f08f29cfbd3c10dde3941
Kevin Paul Lamdrid <lamadridkevinpaul@gmail.com>
2020-02-15 16:21:26 +0800

    Add files via upload
    This first commit for this

---
+++ food/views.py
@@ -0,0 +1,67 @@
+
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Food, Meal
+from .forms import MealForm
+
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Hello, world. You are at the index page.")
+
+def index(request):
+    template = 'list.html'
+    meals = Meal.objects.all() # gets all meal records
+    context = {
+         'meals': meals, #same always
+    }
+    return render(request, template, context)
+
+def add_meal(request):
+    template = "add_meal.html"
+
+    if request.method == "POST":
+        form = MealForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return HttpResponseRedirect(reverse_lazy('food:index'))
+    else:
+        context = {
+			'meal_form': MealForm(),
+		}
+        return render(request, template, context)
+
+def delete_meal(request, meal_id):
+    meal = Meal.objects.get(id=int(meal_id))
+    meal.delete()
+    return HttpResponseRedirect(reverse_lazy('food:index'))
+
+def update_meal(request, meal_id):
+    template = "update_meal.html"
+    meal = Meal.objects.get(id=int(meal_id))
+
+    if request.method == "POST":
+        form = MealForm(request.POST, instance=meal)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse_lazy('food:index'))
+    else:
+        context = {
+            'meal_form': MealForm(instance=meal),
+        }
+    return render(request, template, context)
+
+def view_meal(request, meal_id):
+    meals = Meal.objects.get(id=int(meal_id))
+    template = "view_meal.html"
+    context = {
+             'meals': meals, #same always
+    }
+    return render(request, template, context)
+
+def login(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(reverse_lazy('food:index'))
+    else:
+	    return HttpResponseRedirect(reverse_lazy('auth_login'))
